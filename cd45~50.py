961bdac731b0614fbb3516b36864321b37e58a97
hoonek <karlhkim1@naver.com>
2021-08-19 08:29:57 +0900

    Initial commit

---
+++ cd45~50.py
@@ -0,0 +1,16 @@
+'''
+n = int(input())
+print(n<<1)
+
+a, b = input().split()
+print(int(a) << int(b))
+
+a, b = input().split()
+print(int(a) < int(b))
+
+a, b = input().split()
+print(int(a) == int(b))
+
+a, b = input().split()
+print(int(a) <= int(b))
+'''
---
+++ cd51~60.py
@@ -0,0 +1,7 @@
+'''
+a, b = input().split()
+print(int(a) != int(b))
+
+n = int(input())
+print(bool(n))
+'''
