9efb5e58f5035a13b96ce389445e91cc1f7972e5
hoonek <karlhkim1@naver.com>
2021-08-20 16:27:18 +0900

    cd ~73

---
+++ cd71~80.py
@@ -0,0 +1,22 @@
+'''
+while True:
+  a = int(input())
+  if (a == 0):
+    break
+  print(a)
+
+n = int(input())
+while True:
+  print(n)
+  n = n-1
+  if(n == 0):
+    break
+
+n = int(input())
+while True:
+    n = n - 1
+    print(n)
+
+    if(n == 0):
+     break
+'''
